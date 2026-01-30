4a17c6830f8591c62c30372447cf276995cb0e48
Brian Monsales <brianmonsales@gmail.com>
2026-01-30 23:48:00 +0800

    fix: add the correct ports from env vars to the nginx config

--- docker/nginx/nginx-entrypoint.sh
+++ docker/nginx/nginx-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-envsubst '$VHOST_DOMAIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$VHOST_DOMAIN $FRONTEND_PORT $BACKEND_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
