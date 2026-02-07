9d0ae6bd6ef99c0575df03fec91c1d556df5704c
Brian Monsales <brianmonsales@gmail.com>
2026-02-07 15:55:24 +0800

    update: added nodemon for updating backend in dev env

--- docker/nginx/nginx-entrypoint.sh
+++ docker/nginx/nginx-entrypoint.sh
@@ -2,8 +2,8 @@
 envsubst '$VHOST_DOMAIN $FRONTEND_PORT $BACKEND_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 set -e
 
-# Select config template based on APP_MODE
-if [ "$APP_MODE" = "development" ]; then
+# Select config template based on APP_ENV
+if [ "$APP_ENV" = "development" ]; then
 	TEMPLATE=/etc/nginx/conf.d/nginx.vhost.dev.conf.template
 else
 	TEMPLATE=/etc/nginx/conf.d/nginx.vhost.prod.conf.template
